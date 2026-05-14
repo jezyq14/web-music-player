b4735ca7cc99dc62d2936363454be45305231296
Michał Muzyka <michal.muzyka@proton.me>
2026-05-14 21:27:19 +0200

    Web: change favicon to new logo

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
     import './layout.css';
-    import favicon from '$lib/assets/favicon.svg';
+    import favicon from '$lib/assets/logo.svg';
+
+    import { ModeWatcher } from 'mode-watcher';
 
     let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<ModeWatcher />
+<svelte:head>
+    <link rel="icon" href={favicon} />
+    <title>Michael's Music</title>
+</svelte:head>
 {@render children()}
