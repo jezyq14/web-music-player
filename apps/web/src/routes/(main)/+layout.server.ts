9c5d5fd64f4e7a82829b3f98175558bd0e1a0616
Michał Muzyka <michal.muzyka@proton.me>
2026-05-21 16:18:58 +0200

    Web: main layout update

--- apps/web/src/routes/(main)/+layout.server.ts
+++ apps/web/src/routes/(main)/+layout.server.ts
@@ -1,5 +1,11 @@
 import type { LayoutServerLoad } from './$types';
 
-export const load: LayoutServerLoad = async () => {
-    return {}
+export const load: LayoutServerLoad = async ({ locals }) => {
+    const userFavorites = await (await locals.api.v1.favorites.$get()).json();
+
+    return {
+        user: locals.user,
+        userFavorites,
+
+    };
 };
--- apps/web/src/routes/(main)/+layout.svelte
+++ apps/web/src/routes/(main)/+layout.svelte
@@ -1,49 +1,44 @@
 <script lang="ts">
-    import ThemeToggler from '$lib/components/theme-toggler.svelte';
     import * as Sidebar from '$lib/components/ui/sidebar/index.js';
     import AppSidebar from '$lib/components/app-sidebar.svelte';
-    import { Button } from '$lib/components/ui/button';
-
-    import Logo from '$lib/components/branding/logo.svelte';
-    import SidebarTrigger from '$lib/components/sidebar-trigger.svelte';
     import BottomBar from '$lib/components/player/bottom-bar.svelte';
     import AudioEngine from '$lib/components/player/audio-engine.svelte';
+    import Header from '$lib/components/header.svelte';
+    import { page } from '$app/state';
+    import { IsMobile } from '$lib/hooks/is-mobile.svelte';
+    import { favorites } from '$lib/favorites.svelte';
 
-    let { children } = $props();
+    let { children, data } = $props();
+    const isMobile = new IsMobile().current;
+    let isPlayerPage = $derived(page.url.pathname === '/player');
+
+    $effect.pre(() => {
+        favorites.init(data.userFavorites);
+    });
 </script>
 
 <Sidebar.Provider class="h-dvh">
     <AudioEngine />
+    <AppSidebar
+        class={isPlayerPage && isMobile
+            ? 'h-dvh'
+            : 'h-[calc(100dvh-64px)] md:h-[calc(100dvh-88px)]'}
+    />
 
-    <AppSidebar />
-    <main class="h-full w-full overflow-hidden">
-        <header
-            class="border-border flex h-[54px] w-full items-center justify-between border-b p-2 px-2 md:px-4"
-        >
-            <!-- Left -->
-            <div class="flex items-center gap-2">
-                <SidebarTrigger />
-            </div>
-
-            <!-- Center -->
-            <div class="flex items-center gap-2 md:hidden">
-                <Button href="/" variant="ghost"><Logo class="size-6" /></Button>
-            </div>
-
-            <!-- Right -->
-            <div class="flex items-center gap-2">
-                <ThemeToggler />
-            </div>
-        </header>
-
+    <main class="flex h-full w-full flex-col overflow-hidden">
+        <Header />
         <div
-            class="h-[calc(100dvh-54px-64px)] overflow-auto px-6 py-4 md:h-[calc(100dvh-54px-88px)]"
+            class="overflow-auto px-4 py-4 transition-all md:px-6 {isPlayerPage && isMobile
+                ? 'h-[calc(100dvh-54px)]'
+                : 'h-[calc(100dvh-54px-64px)] md:h-[calc(100dvh-54px-88px)]'}"
         >
             {@render children?.()}
         </div>
     </main>
 
-    <div class="bg-sidebar fixed bottom-0 z-50 h-16 w-full border-t px-4 md:h-22">
-        <BottomBar />
-    </div>
+    {#if !(isPlayerPage && isMobile)}
+        <div class="bg-sidebar fixed bottom-0 z-50 h-16 w-full border-t px-4 md:h-22">
+            <BottomBar />
+        </div>
+    {/if}
 </Sidebar.Provider>
